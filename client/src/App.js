5d3357e7bca3350b86b09bf9ec067cb2c0487297
ludanova <irynaludanova@gmail.com>
2021-12-21 20:48:02 +0200

    Initial structure of project, connect stream.io

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,23 @@
+import { StreamChat } from "stream-chat"
+import { Chat } from "stream-chat-react"
+import Cookies from "universal-cookie"
+import "./App.css"
+import { ChannelContainer, ChannelListContainer } from "./components"
+
+const apiKey = process.env.KEY
+
+const client = StreamChat.getInstance(apiKey)
+
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+
+        <ChannelContainer />
+      </Chat>
+    </div>
+  )
 }
 
 export default App
